5359beee6eae2f41d34719ac366d3fa342621231
raj <sandpriya@gmail.com>
2025-05-26 14:47:58 -0500

    SoftwareEngineer

--- src/main/java/org/example/newspring/Controller/SoftwareEngineerController.java
+++ src/main/java/org/example/newspring/Controller/SoftwareEngineerController.java
@@ -2,21 +2,35 @@ package org.example.newspring.Controller;
 
 import org.example.newspring.Entity.*;
 import org.example.newspring.Service.SoftwareEngineerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.example.newspring.Service.SoftwareEngineerService.*;
 
 @RestController
 @RequestMapping("api/v1/software-engineer")
 public class SoftwareEngineerController {
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getSoftwareEngineer(){
         return SoftwareEngineerService.getALLSoftwareEngineers();
+
+    }
+    @PostMapping
+    public void  addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer){
+        softwareEngineerService.insertSoftwareEngineer( softwareEngineer);
+    }
+    @GetMapping("{id}")
+    public SoftwareEngineer getSoftwareEngineerId(@PathVariable Integer id){
+        return SoftwareEngineerService.getSoftwareEgineerbyID(id);
 
     }
 }
--- src/main/java/org/example/newspring/Service/SoftwareEngineerService.java
+++ src/main/java/org/example/newspring/Service/SoftwareEngineerService.java
@@ -5,6 +5,7 @@ import org.example.newspring.Entity.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SoftwareEngineerService {
@@ -16,5 +17,13 @@ public class SoftwareEngineerService {
     }
     public static List<SoftwareEngineer> getALLSoftwareEngineers(){
         return softwareEngineerRepository.findAll();
+    }
+
+    public static SoftwareEngineer getSoftwareEgineerbyID(Integer id) {
+        return (softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(id + "Not found")));
+    }
+
+    public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer) {
+        softwareEngineerRepository.save(softwareEngineer);
     }
 }
